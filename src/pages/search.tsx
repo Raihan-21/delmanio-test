70ba0624f9e6eaba39a726b0add593b651277903
Muhammad Raihan <muhammadraihan118@gmail.com>
2023-11-24 15:31:42 +0700

    install react window

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,7 +1,17 @@
+import { Box, Flex, Input, Text } from "@chakra-ui/react";
 import React from "react";
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <Box minHeight={"100vh"}>
+      <Text>Search User</Text>
+
+      <Input width={"100%"} marginBottom={5} />
+      <Box border={"1px solid black"}>
+        <Flex justifyContent={"center"} alignItems={"center"}></Flex>
+      </Box>
+    </Box>
+  );
 };
 
 export default Search;
