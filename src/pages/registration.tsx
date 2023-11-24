62177d8927f1cf1a92c0ea8a24ae3f676f1e2e0d
Muhammad Raihan <muhammadraihan118@gmail.com>
2023-11-24 13:10:36 +0700

    add formik as dependency

--- src/pages/registration.tsx
+++ src/pages/registration.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { Box, FormControl, FormLabel, Input, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
 
 const Registration = () => {
-  return <div>Registration</div>;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+  });
+  return (
+    <Box minHeight={"100vh"} padding={10}>
+      <Text fontWeight={"bold"} fontSize={20}>
+        User Registration
+      </Text>
+      <Box>
+        <form action="">
+          <FormControl isInvalid={formData.name === ""}>
+            <FormLabel>Name</FormLabel>
+            <Input />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Email</FormLabel>
+            <Input />
+          </FormControl>
+        </form>
+      </Box>
+    </Box>
+  );
 };
 
 export default Registration;
--- src/pages/users.tsx
+++ src/pages/users.tsx
@@ -1,8 +1,65 @@
 import { Box } from "@chakra-ui/react";
+import axios from "axios";
 import React from "react";
 
-const Users = () => {
-  return <Box>users</Box>;
+export const getServerSideProps = async () => {
+  try {
+    const res = await axios.get(`${process.env.NEXT_PUBLIC_BASE_API}/users`);
+    return {
+      props: {
+        data: res.data.data,
+      },
+    };
+  } catch (error) {}
+};
+
+const Users = ({ data }: { data: any }) => {
+  return (
+    <Box minHeight={"100vh"} padding={10}>
+      Users
+      <table>
+        <thead>
+          <tr>
+            <th>
+              <div className="table-header">Id</div>
+            </th>
+            <th>
+              <div className="table-header">Nama</div>
+            </th>
+            <th>
+              <div className="table-header">Email</div>
+            </th>
+            <th>
+              <div className="table-header">Country Name</div>
+            </th>
+            <th>
+              <div className="table-header">Device Id</div>
+            </th>
+            <th>
+              <div className="table-header">Bitcoin Address</div>
+            </th>
+            <th>
+              <div className="table-header">Avatar</div>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.length > 0 &&
+            data.map((row: any, i: number) => (
+              <tr>
+                <td>{row.id}</td>
+                <td>{row.name}</td>
+                <td>{row.email}</td>
+                <td>{row.country_name}</td>
+                <td>{row.device_id}</td>
+                <td>{row.bitcoin_address}</td>
+                <td>{row.avatar}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </Box>
+  );
 };
 
 export default Users;
